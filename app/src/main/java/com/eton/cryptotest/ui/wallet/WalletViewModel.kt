1e41db8186f6ba669dffb6ae70f84c61bc10ed91
eton <eton@seektopser.com>
2021-09-01 14:48:59 +0800

    add: data binding view

--- app/src/main/java/com/eton/cryptotest/ui/wallet/WalletViewModel.kt
+++ app/src/main/java/com/eton/cryptotest/ui/wallet/WalletViewModel.kt
@@ -1,11 +1,11 @@
 package com.eton.cryptotest.ui.wallet
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.eton.cryptotest.model.Currency
 import com.eton.cryptotest.repository.WalletRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class WalletViewModel : ViewModel() {
@@ -15,17 +15,24 @@ class WalletViewModel : ViewModel() {
     val currencyLiveData = MutableLiveData<ArrayList<Currency>>()
 
     fun getCurrency() {
-        viewModelScope.launch {
-            Log.d("TAG", "getCurrency: ${repository.getCurrencies()}")
+        viewModelScope.launch(Dispatchers.IO) {
             val apiResult = repository.getCurrencies()
             if (apiResult.ok) {
                 for (currency in apiResult.currencies!!) {
                     if (currency.symbol.contentEquals("BTC") ||
-                        currency.symbol.contentEquals("ETH")||
-                        currency.symbol.contentEquals("CRO"))
-                    currencies.add(Currency(currency.colorfulImageUrl, currency.name, "10", "20"))
-                    currencyLiveData.value = currencies
+                        currency.symbol.contentEquals("ETH") ||
+                        currency.symbol.contentEquals("CRO")
+                    )
+                        currencies.add(
+                            Currency(
+                                currency.colorfulImageUrl,
+                                currency.name,
+                                "10",
+                                "20"
+                            )
+                        )
                 }
+                currencyLiveData.postValue(currencies)
             } else {
                 // TODO: 2021/9/1 show error msg
             }
