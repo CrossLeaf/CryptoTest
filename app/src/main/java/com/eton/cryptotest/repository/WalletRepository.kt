e2986052a7af934d17cb43ed9bd23d8e6b8e71a3
eton <eton@seektopser.com>
2021-09-01 17:43:38 +0800

    add: liverates api and walletbalance api

--- app/src/main/java/com/eton/cryptotest/repository/WalletRepository.kt
+++ app/src/main/java/com/eton/cryptotest/repository/WalletRepository.kt
@@ -1,6 +1,8 @@
 package com.eton.cryptotest.repository
 
 import com.eton.cryptotest.model.Currencies
+import com.eton.cryptotest.model.LiveRates
+import com.eton.cryptotest.model.WalletBalance
 import com.eton.cryptotest.source.ApiSource
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -12,6 +14,18 @@ class WalletRepository {
     suspend fun getCurrencies(): Currencies {
         return withContext(Dispatchers.IO) {
             Gson().fromJson(ApiSource.currencies, object : TypeToken<Currencies>() {}.type)
+        }
+    }
+
+    suspend fun getLiveRates(): LiveRates {
+        return withContext(Dispatchers.IO){
+            Gson().fromJson(ApiSource.liveRates, object : TypeToken<LiveRates>() {}.type)
+        }
+    }
+
+    suspend fun getWalletBalance(): WalletBalance {
+        return withContext(Dispatchers.IO){
+            Gson().fromJson(ApiSource.walletBalance, object : TypeToken<WalletBalance>() {}.type)
         }
     }
 
